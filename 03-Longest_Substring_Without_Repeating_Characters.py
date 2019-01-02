570ea779f6738e608703a6577ec87c3cd0746766
tianyuchen <yuchen.tian@edelia.fr>
2019-01-02 17:42:16 +0100

    solution for exo03

---
+++ 03-Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+'''
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+'''
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        maxCount = 0
+        characters = []
+
+        for i in s:
+            if i in characters:
+                maxCount = max(len(characters), maxCount)
+                del characters[0:characters.index(i) + 1]
+
+            characters.append(i)
+
+        maxCount = max(len(characters), maxCount)
+        return maxCount
