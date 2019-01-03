72e8a8b8bd781bcd0e9123127bc06243e2cd06a1
tianyuchen <yuchen.tian@edelia.fr>
2019-01-03 17:57:26 +0100

    solution

--- 07-Reverse_Integer.py
+++ 07-Reverse_Integer.py
@@ -20,3 +20,21 @@ Assume we are dealing with an environment which could only store integers within
 the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem,
 assume that your function returns 0 when the reversed integer overflows.
 '''
+
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        reversedNum = int(str(abs(x))[::-1])
+
+        if x < 0:
+            x = -1 * reversedNum
+        else:
+            x = reversedNum
+
+        if - 2 ** 31 - 1 < x < 2 ** 31:
+            return x
+        else:
+            return 0 
