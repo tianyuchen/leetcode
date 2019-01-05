d147adf97d3dbeb53aeaaa24ffdb070b175a56fc
tianyuchen <tianyuchen0428@gmail.com>
2019-01-05 22:43:08 +0100

    stash

--- 13-Roman_to_Integer.py
+++ 13-Roman_to_Integer.py
@@ -42,3 +42,18 @@ Input: "MCMXCIV"
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 '''
+
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        romanSymbols = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        total = 0
+        for i in s:
+
+            total += romanSymbols[i]
+
+        return total 
