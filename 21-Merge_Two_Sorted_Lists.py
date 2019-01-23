fd6b5a0e6daa7c0b803a67e211a195c5b6574822
tianyuchen <yuchen.tian@edelia.fr>
2019-01-23 17:03:49 +0100

    add comments

--- 21-Merge_Two_Sorted_Lists.py
+++ 21-Merge_Two_Sorted_Lists.py
@@ -8,10 +8,10 @@ Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
 '''
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def mergeTwoLists(self, l1, l2):
@@ -20,15 +20,19 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        sortedList = currentNode = ListNode(0)
-
+        firstNode = currentNode = ListNode(0)
+        # When l1, l2 both are not vide
         while l1 and l2:
             if l1.val <= l2.val:
+                # Link l1 to current node
                 currentNode.next = l1
+                # Move to next item in l1
                 l1 = l1.next
             else:
                 currentNode.next = l2
                 l2 = l2.next
-
+            currentNode = currentNode.next
+        # Link list which isn't empty to current node
         currentNode.next = l1 or l2
-        return currentNode.next
+        # Return sorted list
+        return firstNode.next
