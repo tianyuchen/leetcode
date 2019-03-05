8ad0544401ddcd4ebc0d1109bf385ccf64f666b1
tianyuchen <yuchen.tian@edelia.fr>
2019-03-05 18:08:24 +0100

    add solution

--- 53-Maximum_Subarray.py
+++ 53-Maximum_Subarray.py
@@ -13,3 +13,11 @@ Follow up:
 If you have figured out the O(n) solution, try coding another solution using the
 divide and conquer approach, which is more subtle.
 '''
+
+class Solution:
+    def maxSubArray(self, nums: 'List[int]') -> 'int':
+        max_to_i = max_now = nums[0]
+        for i in nums[1:]:
+            max_to_i = max(i, max_to_i + i)
+            max_now = max(max_now, max_to_i)
+        return max_now
