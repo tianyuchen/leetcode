c103571e0c69467dbf4df2f08299f41d85de58b6
tianyuchen <yuchen.tian@edelia.fr>
2019-03-28 12:01:24 +0100

    solution

---
+++ 58-Length_of_Last_Word.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+'''
+Given a string s consists of upper/lower-case alphabets and empty space
+characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters
+only.
+'''
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        # rstrip(): Return a copy of the string with trailing characters removed.
+        # The chars argument defaults to removing whitespace
+        for i in s.rstrip()[::-1]:
+            if i != ' ':
+                count += 1
+            else:
+                return count
+        return count
+
+
+class Solution2:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.strip().split(' ')[-1])
