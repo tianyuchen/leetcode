0ef4772e8716ac3deca1f2c111e4b76da5aedf96
tianyuchen <tianyuchen0428@gmail.com>
2022-08-23 17:34:32 +0200

    update new solution

--- 69-Sqrt(x).py
+++ 69-Sqrt(x).py
@@ -16,6 +16,27 @@ Input: x = 8
 Output: 2
 Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
 '''
+# Binary search
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+
+        left = 1
+        right = x
+        while left <= right:
+            mid = math.ceil(left + (right - left) / 2)
+            print(left, right, mid)
+            if mid == x/mid:
+                return mid
+            elif mid < x / mid:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
+
+# Brute force
 class Solution:
     def mySqrt(self, x: int) -> int:
         sr = 1
