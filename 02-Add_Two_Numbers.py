fc0a0c6e6239dd509d2121a7efa5828742709937
tianyuchen <tianyuchen0428@gmail.com>
2022-10-18 22:11:58 +0200

    add solution for exo2

--- 02-Add_Two_Numbers.py
+++ 02-Add_Two_Numbers.py
@@ -20,8 +20,28 @@ Explanation: 342 + 465 = 807.
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+class Solution1:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        carry = 0
 
-class Solution:
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            res = v1 + v2 + carry
+            carry = res // 10
+            res = res % 10
+            cur.next = ListNode(res)
+
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
+
+
+class Solution2:
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
