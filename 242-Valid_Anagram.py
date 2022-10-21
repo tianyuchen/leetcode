7963199a88423ac1504ce968566ebc7ff97c1352
tianyuchen <tianyuchen0428@gmail.com>
2022-10-22 00:09:51 +0200

    Solution for exo242

---
+++ 242-Valid_Anagram.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+# -*- coding: utf-8 -*-
+'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        return countS == countT
+            
